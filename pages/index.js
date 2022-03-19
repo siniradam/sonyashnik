732040da5b400be318aa8c162a7c96ecea15551b
Koray K <3178435+siniradam@users.noreply.github.com>
2022-03-19 11:49:51 -0700

    Next Fix 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 import en from "../locale/en";
